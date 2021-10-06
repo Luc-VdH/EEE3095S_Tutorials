04c715416035c00b032d2bd3b328b31ede8bb646
Luc Van Den Handel <vhnluc001@cs.uct.ac.za>
2021-10-06 18:06:18 +0200

    adjustments to get it working and to temperature formula

--- Prac4/prac4.py
+++ Prac4/prac4.py
@@ -1,4 +1,5 @@
 import busio
+import RPi.GPIO as GPIO
 import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
@@ -7,7 +8,7 @@ import time
 import threading
 
 #inital thread delay
-delay=10.0
+delay=1.0
 
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -26,23 +27,16 @@ chan2 = AnalogIn(mcp, MCP.P2)
 
 def setup():
     # Setup board mode
-    GPIO.setmode(GPIO.BOARD)
+    # GPIO.setmode(GPIO.BOARD)
     # setup the push button
-    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(23, GPIO.FALLING, callback=btn_pressed, bouncetime=200)
+    GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(26, GPIO.FALLING, callback=btn_pressed, bouncetime=200)
 
 
-if __name__ == "__main__":
-    setup()
-    print("{0:<20} {1:<20} {2:<20} {3:<20}".format("Runtime","Temp Reading", "Temp", "Light Reading"))
-    startTime= time.time()
-    while True:
-        thread = threading.Thread(target=print_result, args=(delay,))
-        thread.start()
-        thread.join()
 
 #changes the delay time of the thread
-def btn_pressed():
+def btn_pressed(channel):
+    global delay
     if delay == 10.0:
         delay=5.0
     elif delay == 5.0:
@@ -51,11 +45,24 @@ def btn_pressed():
         delay=10.0
 	
 def print_result():
-    time.sleep(delay)
+    global delay
+    global chan1
+    global chan2
     n=time.time()-startTime
-    seconds=n+"s"
+    seconds= str(round(n,0)) + "s"
     #sensor output at 0 degrees(V0) is 500V and the temperature co-efficient(Tc) is 10
     #temp=(Vdata-V0)/Tc
-    temp=(chan1.value-500)/10
-    print("{0:<20.0f} {1:<20} {2:<20}C {3:<20}".format(seconds,chan1.value, temp, chan2.value))
+    voltage = chan1.value * (5/1023.0)
+    t = (voltage - 0.5)
+    temp=(chan1.value-500)/100
+    print("{0:<20} {1:<20} {2:<20}C {3:<20}".format(seconds,chan1.value, t, chan2.value))
+    time.sleep(delay)
     
+if __name__ == "__main__":
+    setup()
+    print("{0:<20} {1:<20} {2:<20} {3:<20}".format("Runtime","Temp Reading", "Temp", "Light Reading"))
+    startTime= time.time()
+    while True:
+        thread = threading.Thread(target=print_result)
+        thread.start()
+        thread.join()
